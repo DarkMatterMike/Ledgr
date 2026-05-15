3f1bb9501e77d26a727a5a98cb4a7a198a761ce3
Mike Fettig <michaeljfettig@gmail.com>
2026-05-14 20:41:37 -0400

    fix crash

--- frontend/src/components/LedgrCalendar.jsx
+++ frontend/src/components/LedgrCalendar.jsx
@@ -573,7 +573,6 @@ export default function LedgrCalendar({
                   );
                 })}
               </div>
-            </div>
               {/* Mobile day bottom sheet */}
               {isMobile&&sheetOpen&&(
                 <>
